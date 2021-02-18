7a6e173fc82cb7f271c90be8e555ae4eb83b521b
Abdellah Hallou <61630241+ABDELLAH-Hallou@users.noreply.github.com>
2021-02-18 20:23:35 +0100

    Update db_connect.php

--- Projet PHP/config/db_connect.php
+++ Projet PHP/config/db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 //connect to db
-$conn = mysqli_connect('localhost', 'root' , 'Abdo@2020' , 'php');
+$conn = mysqli_connect('localhost', 'root' , '' , 'php');
 	// check the connection 
 	if(!$conn){
 		echo 'connection error :'. mysqli_connect_error();
